d904de72dca5297d34e40c341de526d8bd64a30b
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2021-07-26 23:08:17 +0100

    Update server/routes/routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import compression from 'compression';
+import responseTime from 'response-time';
+import rateLimit from 'express-rate-limit';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from '../config/swagger';
 import winston from '../config/winston';
@@ -12,8 +15,15 @@ import order from './order';
 import admin from './admin';
 
 export default (app) => {
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  });
   app.use(cors());
   app.use(helmet());
+  app.use(compression());
+  app.use(responseTime());
+  app.use(limiter);
   app.use(morgan('combined', { stream: winston.stream }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
@@ -24,7 +34,17 @@ export default (app) => {
   app.use('/api/v1/users', admin);
   app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   app.use('/api/v1$', (req, res) => {
-    res.status(200).json({ status: 200, message: 'Welcome to E-commerce application', version: '1.0.0' });
+    res.status(200).json({
+      status: 200,
+      message: 'Welcome to E-commerce application',
+      version: '1.0.0',
+      author: 'Okwuidegbe Emmanuel',
+      email: 'okwuidegbeemmauel@gmail.com',
+      about: 'A simple E-commerce application.',
+    });
   });
-  app.use((req, res) => res.status(404).json({ status: 404, message: 'Hello, page not found' }));
+  app.use((req, res) => res.status(404).json({
+    status: 404,
+    message: 'Hello, page not found. Try connecting using an api testing application like POSTMAN',
+  }));
 };
